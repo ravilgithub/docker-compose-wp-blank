5c8084963875f808a75909224783aaa3ce59082b
Ravil <foragetonly800@mail.ru>
2024-05-26 02:46:50 +0300

    Настроить MailHog
    Нужно сообщить WordPress, чтобы он отправлял все письма
    в службу "mailhog".
    Источник:
    https://bonnick.dev/posts/developing-wordpress-with-docker

--- theme/functions.php
+++ theme/functions.php
@@ -24,3 +24,15 @@ function example_styles() {
 	);
 }
 add_action( 'wp_enqueue_scripts', 'example_styles' );
+
+add_action('phpmailer_init', function ($php_mailer) {
+	$php_mailer->Host     = 'mailhog';
+	$php_mailer->Port     = 1025;
+	$php_mailer->From     = 'wordpress@localhost.test';
+	$php_mailer->FromName = 'WordPress';
+	$php_mailer->IsSMTP();
+}, 10);
+
+add_filter('wp_mail_from', fn ($email) => 'wordpress@localhost.test');
+
+add_filter('wp_mail_from_name', fn ($name) => 'localhost');
